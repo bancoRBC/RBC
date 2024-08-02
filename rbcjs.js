c8148c08d145ee910ec1b51611661ba08b3f86c5
RBC <aroarosegundo2@gmail.com>
2024-08-02 17:41:23 -0500

    rbcjs.js

--- rbcjs.js
+++ rbcjs.js
@@ -10,6 +10,9 @@ function showInfo() {
         window.location.href = "descargas2.html"
     } else if (number == 525905) {
         window.location.href = "descargas3.html"
+    } else if (number == 236726
+) {
+        window.location.href = "descargas4.html"
     } else {
         infoDisplay.innerHTML = "<p>Información no disponible para este número.</p>";
     }
