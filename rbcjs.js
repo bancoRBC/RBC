fa02c06773b62f2adc1a3780cf236c037d73d3ae
RBC <aroarosegundo2@gmail.com>
2024-08-02 15:00:03 -0500

    Add files via upload

---
+++ rbcjs.js
@@ -0,0 +1,16 @@
+function showInfo() {
+    var number = document.getElementById('numberInput').value;
+    var infoDisplay = document.getElementById('infoDisplay2');
+    
+    if (number == 101598
+    ) {
+        window.location.href = "/descargas.html"
+    } else if (number == 815776
+    ) {
+        window.location.href = "descargas2.html"
+    } else if (number == 525905) {
+        window.location.href = "descargas3.html"
+    } else {
+        infoDisplay.innerHTML = "<p>Información no disponible para este número.</p>";
+    }
+}
