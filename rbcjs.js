53ef7b083e50718da4a9a476b292aef222965a5c
RBC <aroarosegundo2@gmail.com>
2024-08-02 18:11:45 -0500

    rbcjs.js

--- rbcjs.js
+++ rbcjs.js
@@ -5,16 +5,13 @@ function showInfo() {
     if (number == 101598
     ) {
         window.location.href = "/descargas.html"
-    } else if (number == 815776
-    ) {
+    } else if (number == 815776) {
         window.location.href = "descargas2.html"
     } else if (number == 525905) {
         window.location.href = "descargas3.html"
-    } else if (number == 236726
-) {
+    } else if (number == 236726) {
         window.location.href = "descargas4.html"
-    } else if (number == 390872
-) {
+    } else if (number == 390872) {
         window.location.href = "descargas5.html"
     } else if (number == 131851) {
         window.location.href = "descargas6"
